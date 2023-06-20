e9f07f72c73c1c106cbe66d91830888357c84bec
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-06-20 08:24:42 -0300

    começando a adicionar o js

--- script/script.js
+++ script/script.js
@@ -4,11 +4,5 @@ const bnt = document.getElementById("submit-bnt")
 
 
 bnt.addEventListener("click", () => {
-    const emailValue = email.value 
-
-    if (emailValue === "") {
-        alert("Coloca um email valido animal")
-    } else {
-        alert("Muito bom")
-    }
+    
 })
