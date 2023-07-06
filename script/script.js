e15d97a9eced7636179a26ab0462d9d45666b3ff
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-07-06 18:50:16 -0300

    .

--- script/script.js
+++ script/script.js
@@ -1,1 +1,34 @@
+const form = document.getElementsByClassName('form')
+const email = document.getElementById('email')
 
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInput();
+});
+
+function checkInput() {
+    const emailValue = email.value;
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Please provide a valid email');
+    } else {
+        
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+}
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+}
