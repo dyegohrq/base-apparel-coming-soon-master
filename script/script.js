07d56ca4477a5390c2417e70722791dc18831cab
José Dyego Henrique Cardoso de Sousa <dyegoh408@gmail.com>
2023-07-07 15:33:53 -0300

    end of project

--- script/script.js
+++ script/script.js
@@ -1,32 +1,36 @@
 const form = document.getElementById("form")
 const email = document.getElementById("email")
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkInput();
 });
 
 function checkInput() {
-    const emailValue = email.value;
+    const emailValue = email.value 
 
-    if (emailValue === "") {
-        setErrorFor(email, 'Please provide a valid email.');
+    if (emailValue === '') {
+        setErrorFor(email, 'Looks like this is not an email.')
     } else if (!checkEmail(emailValue)) {
-        setErrorFor(email, 'Please enter a valid email.');
+        setErrorFor(email, 'Please provide a valid email.')
+    } else {
+        setSuccessFor(email)
     }
 }
 
-
-
-
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
+    const small = document.querySelector('small');
 
+    formControl.className = 'form-control error';
     small.innerText = message;
+}
 
-    formControl.className = "form-control error";
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
 }
 
 function checkEmail(email) {
